3ab83dadea7e7102f1465bc27fdf28ef713ac2bf
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2023-12-04 10:38:52 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ function App() {
 
   return (
     <>
-     <h1>Hello world!</h1>
+      <h1>Hello world!</h1>
+      <h1>Hello world!</h1>
+      <h1>Hello world!</h1>
+      <h1>Hello world!</h1>
     </>
-  )
+  );
 }
 
 export default App
